b14e19cfeff399f81bced4f8d900b73df92dc874
inakicarcereny <inakicarce@hotmail.com.ar>
2024-06-05 10:29:08 -0300

    functional cart

--- src/components/home/HeaderHome.jsx
+++ src/components/home/HeaderHome.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { CartIcon } from "../../icons/CartIcon";
 
 export function HeaderHome() {
@@ -9,9 +10,9 @@ export function HeaderHome() {
       <span className="font-semibold text-zinc-500">Hello Inaki, welcome back!</span>
     </div>
     <div className="flex items-center">
-      <span className="bg-white dark:bg-[#252525] px-1 py-1 shadow-xl rounded-full hover:scale-110 duration-300 ease-in-out cursor-pointer">
+      <Link to='/cart' className="bg-white dark:bg-[#252525] px-1 py-1 shadow-xl rounded-full hover:scale-110 duration-300 ease-in-out cursor-pointer">
         <CartIcon />
-      </span>
+      </Link>
     </div>
   </header>
   )
